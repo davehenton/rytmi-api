f526039054a732a6a1e50002d6b2abc2a6075141
Lauri Kajan <lkajan@fizzbuzz.local>
2018-02-07 14:07:34 +0200

    separate app and server

--- api/index.js
+++ api/index.js
@@ -1,19 +1,9 @@
-import http from 'http'
-import express from 'express'
-import api from './api'
+
 import cors from 'cors'
-require('dotenv').config()
 
-let app = express()
-app.server = http.createServer(app)
-
+const app = require('./app')
 app.use(cors())
 
-// api router
-app.use('/api', api())
-
-app.server.listen(process.env.PORT, () => {
-  console.log(`Started on port ${app.server.address().port}`)
+let server = app.listen(process.env.PORT, () => {
+  console.log(`Started on port ${server.address().port}`)
 })
-
-export default app
